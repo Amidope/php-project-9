de01805c78f1d51783b1dbfc21a5dcca6eab84be
Amidope <noreply>
2023-12-01 21:05:37 +0300

    update

--- public/index.php
+++ public/index.php
@@ -2,14 +2,22 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$twig = Twig::create( '../templates', ['debug' => true]);
+$app->add(TwigMiddleware::create($app, $twig));
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->get('/', function (Request $request, Response $response, $args) use ($twig) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.twig', [
+        'linkMainActive' => 'active'
+    ]);
+})->setName('root');
+
+
 
 $app->run();
