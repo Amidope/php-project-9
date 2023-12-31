7d1587b3218cfb116a1541dbf4300b36ec00522b
Amidope <noreply>
2023-12-31 20:49:07 +0500

    update

--- src/Connection.php
+++ src/Connection.php
@@ -9,7 +9,6 @@ class Connection
     public function connect()
     {
 //      "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s"
-//      $url = "postgres://root:99AChTwD1MsC8l7PmehZI7XJgAgl9Nrk@dpg-cli8re6f27hc73a02s50-a.oregon-postgres.render.com/analyzerdb_xz6b";
 
         $databaseUrl = parse_url($_ENV['DATABASE_URL']);
         $databaseUrl['port'] = $databaseUrl['port'] ?? '5432';
