4a16af9fac1aa5641911892e7b0631253f95b36e
Amidope <noreply>
2024-08-13 17:38:55 +0500

    load dotenv

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ use function Amidope\PageAnalyzer\buildApp;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
+$dotenv->load();
+
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
