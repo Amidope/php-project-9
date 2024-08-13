4efb41bd2ac397c7343e476189f7010c780344d7
Amidope <noreply>
2024-08-13 20:04:03 +0500

    fix dotenv path

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use function Amidope\PageAnalyzer\buildApp;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createMutable(dirname(__DIR__));
 $dotenv->load();
 
 if (session_status() !== PHP_SESSION_ACTIVE) {
