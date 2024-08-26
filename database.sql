b4f8b031464578735ff560c12fa71811afe5f015
Amidope <noreply>
2024-08-26 21:00:53 +0500

    fix sql

--- database.sql
+++ database.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS urls;
 CREATE TABLE urls
 (
     id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name       varchar(255),
-    created_at TIMESTAMP
+    name       varchar(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
 );
 
 
@@ -14,9 +14,9 @@ CREATE TABLE url_checks
 (
     id          bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id      bigint REFERENCES urls (id),
-    status_code int,
-    h1          varchar(255),
-    title       varchar(255),
-    description varchar(255),
-    created_at  TIMESTAMP
+    status_code int NOT NULL,
+    h1          varchar(1000) NOT NULL,
+    title       text NOT NULL,
+    description text,
+    created_at  TIMESTAMP NOT NULL
 );
