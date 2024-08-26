3fc009789c320ebd6b76d60da275f8caa9a826b8
Amidope <noreply>
2024-08-26 18:39:44 +0500

    safe load .env

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use function Amidope\PageAnalyzer\buildApp;
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createMutable(dirname(__DIR__));
-$dotenv->load();
+$dotenv->safeLoad();
 
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
