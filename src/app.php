74f5f44e152ded129bd1322f17171da30c42258c
Amidope <noreply>
2024-08-29 16:39:37 +0500

    refactor

--- src/app.php
+++ src/app.php
@@ -4,16 +4,23 @@ namespace Amidope\PageAnalyzer;
 
 use Slim\App;
 
+/**
+ * @param \Slim\App<\Psr\Container\ContainerInterface> $app
+ * @return void
+ */
 function registerRoutes(App $app): void
 {
     require __DIR__ . '/../config/routes.php';
 }
+
+/**
+ * @return \Slim\App<\Psr\Container\ContainerInterface>
+ */
 function buildApp(): App
 {
     $builder = new \DI\ContainerBuilder();
     $builder->addDefinitions(__DIR__ . '/../config/dependencies.php');
     $container = $builder->build();
-    /** @var \Slim\App<\Psr\Container\ContainerInterface> $app */
     $app = $container->get('app');
     registerRoutes($app);
     return $app;
